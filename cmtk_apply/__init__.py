3b58a4045ba6921bd41872217f871f1742173f3d
Philipp Schlegel <observing@web.de>
2026-02-11 11:07:47 -0500

    add __version__

--- cmtk_apply/__init__.py
+++ cmtk_apply/__init__.py
@@ -1,3 +1,7 @@
 from .registration import Registration, RegistrationChain, load_registration
 
+from importlib.metadata import version as _version
+
+__version__ = _version("cmtk_apply")
+
 __all__ = ["Registration", "RegistrationChain", "load_registration"]
